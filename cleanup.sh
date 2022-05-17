46fd0c321c1827134fda637ef70e5eb126035ccc
ggalloro <galloro.giovanni@gmail.com>
2022-05-17 11:18:41 +0200

    WIP

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 rm -rf workspace/artifacts.json
 gcloud deploy delivery-pipelines delete progressive-pipeline --region europe-west1 --force --quiet
 gcloud artifacts docker images delete europe-docker.pkg.dev/galloro-demos/cloudbuilds/leeroy-app --quiet
+kubectl -n progress delete deploy,svc --all
 gcloud deploy apply --file=clouddeploy-config/delivery-pipeline.yaml --region=europe-west1 --project=galloro-demos --quiet
